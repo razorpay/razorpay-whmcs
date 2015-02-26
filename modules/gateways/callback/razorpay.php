b02835a5a4a079245f11fbcb8f98c0c8c4e2a2f8
Harshil Mathur <harshilmathur@gmail.com>
2015-02-25 17:20:06 -0800

    Initial Version

---
+++ modules/gateways/callback/razorpay.php
@@ -0,0 +1,103 @@
+<?php
+
+# Required File Includes
+include("../../../dbconnect.php");
+include("../../../includes/functions.php");
+include("../../../includes/gatewayfunctions.php");
+include("../../../includes/invoicefunctions.php");
+
+$gatewaymodule = "razorpay";
+
+$GATEWAY = getGatewayVariables($gatewaymodule);
+
+# Checks gateway module is active before accepting callback
+if (!$GATEWAY["type"])
+    die("Module Not Activated");
+
+$key_id = $GATEWAY["KeyId"];
+$key_secret = $GATEWAY["KeySecret"];
+
+
+# Get Returned Variables
+$merchant_order_id = $_POST["merchant_order_id"];
+$razorpay_payment_id = $_POST["razorpay_payment_id"];
+
+# Checks invoice ID is a valid invoice number or ends processing
+$merchant_order_id = checkCbInvoiceID($merchant_order_id, $GATEWAY["name"]); 
+
+# Checks transaction number isn't already in the database and ends processing if it does
+checkCbTransID($razorpay_payment_id); 
+
+# Fetch invoice to get the amount
+$result = mysql_fetch_assoc(select_query('tblinvoices','total',array("id"=>$merchant_order_id))); 
+$amount = $result['total'];
+
+# Check if amount is INR, convert if not.
+$currency = getCurrency();
+if($currency['code'] !== 'INR') {
+    $result = mysql_fetch_array(select_query( "tblcurrencies", "id", array( "code" => 'INR' )));
+    $inr_id= $result['id'];
+    $converted_amount = convertCurrency($amount,$currency['id'], $inr_id);
+}
+else {
+    $converted_amount = $amount;
+}
+
+# Amount in Paisa
+$converted_amount = 100*$converted_amount;
+
+$success = true;
+$error = "";
+
+try {
+    $url = 'https://api.razorpay.com/v1/payments/'.$razorpay_payment_id.'/capture';
+    $fields_string="amount=$converted_amount";
+
+    //cURL Request
+    $ch = curl_init();
+
+    //set the url, number of POST vars, POST data
+    curl_setopt($ch,CURLOPT_URL, $url);
+    curl_setopt($ch,CURLOPT_USERPWD, $key_id . ":" . $key_secret);
+    curl_setopt($ch,CURLOPT_TIMEOUT, 60);
+    curl_setopt($ch,CURLOPT_POST, 1);
+    curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER, TRUE);
+
+    //execute post
+    $result = curl_exec($ch);
+    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    $array = json_decode($result, true);
+
+    //close connection
+    curl_close($ch);
+
+    //Check success response
+    if($http_status === 200 and isset($array['error']) === false){
+        $success = true;    
+    }
+    else {
+        $error = $array['error']['code'].":".$array['error']['description'];
+        $success = false;
+    }
+}
+catch (Exception $e) {
+    $success = false;
+    $error ="WHMCS_ERROR:Request to Razorpay Failed";
+}
+
+if ($success === true) {
+    # Successful 
+    # Apply Payment to Invoice: invoiceid, transactionid, amount paid, fees, modulename
+    addInvoicePayment($merchant_order_id, $razorpay_payment_id, $amount, 0, $GATEWAY["name"]);
+    logTransaction($GATEWAY["name"], $_POST, "Successful"); # Save to Gateway Log: name, data array, status
+} 
+else {
+    # Unsuccessful
+    # Save to Gateway Log: name, data array, status
+    logTransaction($GATEWAY["name"], $_POST, "Unsuccessful-".$error . ". Please check razorpay dashboard for Payment id: ".$_POST['razorpay_payment_id']);
+}
+
+header( "Location: ".$GATEWAY['systemurl']."/viewinvoice.php?id=" . $merchant_order_id );
+?>
---
+++ modules/gateways/razorpay.php
@@ -0,0 +1,79 @@
+<?php
+function razorpay_config() {
+
+    $configarray = array(
+        "FriendlyName" => array("Type" => "System", "Value" => "Razorpay"),
+        "KeyId" => array("FriendlyName" => "Key Id", "Type" => "text", "Size" => "50", "Description" => "Enter your Razorpay Key Id here",),
+        "KeySecret" => array("FriendlyName" => "Key Secret", "Type" => "text", "Size" => "50", "Description" => "Enter your Razorpay Key Secret here",),
+    );
+    return $configarray;
+}
+
+function razorpay_link($params) {
+    # Gateway Specific Variables
+    $key_id = $params['KeyId'];
+    $key_secret = $params['KeySecret'];  
+
+    # Invoice Variables
+    $order_id = $params['invoiceid'];
+    $description = $params["description"];
+    $amount = $params['amount']*100;
+    $currency = $params['currency'];
+    
+    # Client Variables
+    $customer_name = $params['clientdetails']['firstname']." ".$params['clientdetails']['lastname'];
+    $customer_email = $params['clientdetails']['email'];
+    $customer_phone = $params['clientdetails']['phonenumber'];
+    
+    # System Variables
+    $name = $params['companyname'];
+    $companyname = 'razorpay';
+    $checkoutURL = 'https://checkout.razorpay.com/v1/checkout.js';
+    $callbackURL = $params['systemurl'].'/modules/gateways/callback/razorpay.php';
+
+
+    $html = '<form name="razorpay-form" id="razorpay-form" action="'.$callbackURL.'" method="POST" onSubmit="if(!razorpay_open) razorpaySubmit(); return razorpay_submit;">
+                <input type="hidden" name="razorpay_payment_id" id="razorpay_payment_id" />
+                <input type="hidden" name="merchant_order_id" id="order_id" value="'.$order_id.'"/>
+                <input type="button" value="Click Here to Pay" onClick="razorpaySubmit()"/>
+            </form>';
+    
+    $js = '<script src="'.$checkoutURL.'"></script>';
+
+    $js .= "<script>
+            var razorpay_open = false;
+            var razorpay_submit = false;
+            var razorpay_options = {
+                'key': '".$key_id."',
+                'amount': '".$amount."',
+                'currency': '".$currency."',
+                'name': '".$name."',
+                'description': '".$description."',
+                'handler': function (transaction) {
+                    razorpay_submit = true;
+                    document.getElementById('razorpay_payment_id').value = transaction.razorpay_payment_id;
+                    document.getElementById('razorpay-form').submit();
+                },
+                'prefill': {
+                    'name': '".$customer_name."',
+                    'email': '".$customer_email."',
+                    'contact': '".$customer_phone."'
+                },
+                notes: {
+                    'whmcs_order_id': '".$order_id."'
+                },
+                netbanking: true
+            };
+            
+            function razorpaySubmit(){                  
+                var rzp1 = new Razorpay(razorpay_options);
+                rzp1.open();
+                razorpay_open = true;
+            }    
+
+            </script>";
+
+    return $html.$js;
+
+}
+?>
