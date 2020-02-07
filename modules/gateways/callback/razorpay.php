9edb9cbe8d71db024b670644131a38808f65aff8
ChetanGN <chetan.naik@razorpay.com>
2020-02-07 16:10:21 +0530

    PON-189 [WHMCS] Stabilize the razorpay-WHMCS plugin with auto-capture

--- modules/gateways/callback/razorpay.php
+++ modules/gateways/callback/razorpay.php
@@ -11,90 +11,115 @@
 require_once __DIR__ . '/../../../init.php';
 require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
 require_once __DIR__ . '/../../../includes/invoicefunctions.php';
+require_once __DIR__.'/../razorpay-sdk/Razorpay.php';
+
+use Razorpay\Api\Api;
+use Razorpay\Api\Errors;
 
 // Detect module name from filename.
 $gatewayModuleName = 'razorpay';
+
 // Fetch gateway configuration parameters.
 $gatewayParams = getGatewayVariables($gatewayModuleName);
+
 // Die if module is not active.
-if (!$gatewayParams['type']) {
+if (!$gatewayParams['type'])
+{
     die("Module Not Activated");
 }
-$keyId = $gatewayParams["keyId"];
-$keySecret = $gatewayParams["keySecret"];
+
 // Retrieve data returned in payment gateway callback
-$merchant_order_id = $_POST["merchant_order_id"];
+$merchant_order_id   = $_POST["merchant_order_id"];
 $razorpay_payment_id = $_POST["razorpay_payment_id"];
+
+$success = false;
+$error = "";
+
+try
+{
+    verifySignature($merchant_order_id, $_POST, $gatewayParams);
+    $success = true;
+}
+catch (Errors\SignatureVerificationError $e)
+{
+    $error = 'WHMCS_ERROR: Payment to Razorpay Failed. ' . $e->getMessage();
+}
+
+/**
+* @codeCoverageIgnore
+*/
+function getApiInstance($key,$keySecret)
+{
+    return new Api($key, $keySecret);
+}
+/**
+ * Verify the signature on payment success
+ * @param  int $order_no
+ * @param  array $response
+ * @param  array $gatewayParams
+ * @return
+ */
+function verifySignature(int $order_no, array $response, $gatewayParams)
+{
+    $api = getApiInstance($gatewayParams['keyId'], $gatewayParams['keySecret']);
+
+    $attributes = array(
+        RAZORPAY_PAYMENT_ID => $response[RAZORPAY_PAYMENT_ID],
+        RAZORPAY_SIGNATURE  => $response[RAZORPAY_SIGNATURE],
+    );
+
+    $sessionKey = getOrderSessionKey($order_no);
+
+    $attributes[RAZORPAY_ORDER_ID] = $_SESSION[$sessionKey];
+
+    $api->utility->verifyPaymentSignature($attributes);
+}
+
 // Validate Callback Invoice ID.
 $merchant_order_id = checkCbInvoiceID($merchant_order_id, $gatewayParams['name']);
+
 // Check Callback Transaction ID.
 checkCbTransID($razorpay_payment_id);
+
 /**
  * Fetch amount to verify transaction
  */
 # Fetch invoice to get the amount and userid
-$result = mysql_fetch_assoc(select_query('tblinvoices', 'total, userid', array("id"=>$merchant_order_id))); 
+$result = mysql_fetch_assoc(select_query('tblinvoices', 'total, userid', array("id"=>$merchant_order_id)));
+
 $amount = $result['total'];
+
 # Check if amount is INR, convert if not.
 //$currency = getCurrency();
 $result = mysql_fetch_assoc(select_query('tblclients', 'currency', array("id"=>$result['userid'])));
+
 $currency_id = $result['currency'];
+
 $result = mysql_fetch_array(select_query("tblcurrencies", "id", array("code"=>'INR')));
+
 $inr_id = $result['id'];
-if($currency_id != $inr_id) {
+
+if($currency_id != $inr_id)
+{
     $converted_amount = convertCurrency($amount, $currency_id, $inr_id);
-} else {
+}
+else
+{
     $converted_amount = $amount;
 }
+
 # Amount in Paisa
 $converted_amount = 100*$converted_amount;
-$success = true;
-$error = "";
-try {
-    $url = 'https://api.razorpay.com/v1/payments/'.$razorpay_payment_id.'/capture';
-    $fields_string="amount=$converted_amount";
-    //cURL Request
-    $ch = curl_init();
-    //set the url, number of POST vars, POST data
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_USERPWD, $keyId . ":" . $keySecret);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    //execute post
-    $result = curl_exec($ch);
-    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    if ($result === false) {
-        $success = false;
-        $error = 'Curl error: ' . curl_error($ch);
-    } else {
-        $response_array = json_decode($result, true);
-        //Check success response
-        if ($http_status === 200 and isset($response_array['error']) === false) {
-            $success = true;    
-        } else {
-            $success = false;
-            if (!empty($response_array['error']['code'])) {
-                $error = $response_array['error']['code'].":".$response_array['error']['description'];
-            } else {
-                $error = "RAZORPAY_ERROR: Invalid Response <br/>".$result;
-            }
-        }
-    }
-        
-    //close connection
-    curl_close($ch);
-} catch (Exception $e) {
-    $success = false;
-    $error ="WHMCS_ERROR: Request to Razorpay Failed";
-}
-if ($success === true) {
+
+if ($success === true)
+{
     # Successful
     # Apply Payment to Invoice: invoiceid, transactionid, amount paid, fees, modulename
     addInvoicePayment($merchant_order_id, $razorpay_payment_id, $amount, 0, $gatewayParams["name"]);
     logTransaction($gatewayParams["name"], $_POST, "Successful"); # Save to Gateway Log: name, data array, status
-} else {
+}
+else
+{
     # Unsuccessful
     # Save to Gateway Log: name, data array, status
     logTransaction($gatewayParams["name"], $_POST, "Unsuccessful-".$error . ". Please check razorpay dashboard for Payment id: ".$_POST['razorpay_payment_id']);
--- modules/gateways/razorpay.php
+++ modules/gateways/razorpay.php
@@ -1,5 +1,19 @@
 <?php
 
+require_once __DIR__.'/razorpay-sdk/Razorpay.php';
+
+use Razorpay\Api\Api;
+use Razorpay\Api\Errors;
+
+
+const RAZORPAY_WHMCS_VERSION= '2.0';
+const RAZORPAY_PAYMENT_ID   = 'razorpay_payment_id';
+const RAZORPAY_ORDER_ID     = 'razorpay_order_id';
+const RAZORPAY_SIGNATURE    = 'razorpay_signature';
+
+const CAPTURE            = 'capture';
+const AUTHORIZE          = 'authorize';
+const WHMCS_ORDER_ID     = 'whmcs_order_id';
 /**
  * WHMCS Razorpay Payment Gateway Module
  */
@@ -23,6 +37,13 @@ function razorpay_MetaData()
  */
 function razorpay_config()
 {
+    $gatewayModuleName = 'razorpay';
+
+    // Fetch gateway configuration parameters.
+    $gatewayParams = getGatewayVariables($gatewayModuleName);
+
+    $webhookUrl = $gatewayParams['systemurl']; //TO DO: change the webhook URL once script implemented.
+
     return array(
         // the friendly display name for a payment gateway should be
         // defined here for backwards compatibility
@@ -41,9 +62,90 @@ function razorpay_config()
             'Type' => 'text',
             'Size' => '50',
             'Description' => 'Razorpay "Key Secret" shared during activation API Key',
+        ),
+        'paymentAction' => array(
+            'FriendlyName' => 'Payment Action',
+            'Type' => 'dropdown',
+            'Default' => 'Authorize and Capture',
+            'Options' => array(
+                CAPTURE   => 'Authorize and Capture',
+                AUTHORIZE => 'Authorize',
+            ),
+            'Description' => 'Payment action on order compelete.',
+        ),
+        'enableWebhook' => array(
+            'FriendlyName' => 'Enable Webhook',
+            'Type' => 'yesno',
+            'Default' => false,
+            'Description' => 'Enable Razorpay Webhook <a href="https://dashboard.razorpay.com/#/app/webhooks">here</a> with the URL listed below. <br/><span>'.$webhookUrl.'</span><br/><br/>Instructions and guide to <a href="https://github.com/razorpay/razorpay-woocommerce/wiki/Razorpay-Woocommerce-Webhooks">Razorpay webhooks</a>',
+        ),
+        'webhookSecret' => array(
+            'FriendlyName' => 'Webhook Secret',
+            'Type' => 'text',
+            'Size' => '50',
+            'Description' => '<br/> Webhook secret is used for webhook signature verification. This has to match the one added <a href="https://dashboard.razorpay.com/#/app/webhooks">here</a>',
         )
     );
 }
+
+/**
+* @codeCoverageIgnore
+*/
+function getRazorpayApiInstance($params)
+{
+    $key    = $params['keyId'];
+    $secret = $params['keySecret'];
+
+    return new Api($key, $secret);
+}
+
+/**
+ * Create the session key name
+ * @param  int $order_no
+ * @return
+ */
+function getOrderSessionKey($order_no)
+{
+    return RAZORPAY_ORDER_ID . $order_no;
+}
+
+/**
+ * Create razorpay order id
+ * @param  array  $params
+ * @return string
+ */
+function createRazorpayOrderId(array $params)
+{
+    $api = getRazorpayApiInstance($params);
+
+    $data = array(
+        'receipt'         => $params['invoiceid'],
+        'amount'          => (int) round($params['amount'] * 100),
+        'currency'        => $params['currency'],
+        'payment_capture' => ($params['paymentAction'] === AUTHORIZE) ? 0 : 1,
+        'notes'           => array(
+            WHMCS_ORDER_ID  => (string) $params['invoiceid'],
+        ),
+    );
+
+    try
+    {
+        $razorpayOrder = $api->order->create($data);
+    }
+    catch (Exception $e)
+    {
+        return $e;
+    }
+
+    $razorpayOrderId = $razorpayOrder['id'];
+
+    $sessionKey = getOrderSessionKey($params['invoiceid']);
+
+    $_SESSION[$sessionKey] = $razorpayOrderId;
+
+    return $razorpayOrderId;
+}
+
 /**
  * Payment link.
  * Required by third party payment gateway modules only.
@@ -56,22 +158,25 @@ function razorpay_link($params)
 {
     // Gateway Configuration Parameters
     $keyId = $params['keyId'];
-    
+
     // Invoice Parameters
     $invoiceId = $params['invoiceid'];
     $description = $params["description"];
     $amount = $params['amount'] * 100; // Required to be converted to Paisa.
     $currencyCode = $params['currency'];
-    
+
     // Client Parameters
     $name = $params['clientdetails']['firstname'].' '.$params['clientdetails']['lastname'];
     $email = $params['clientdetails']['email'];
     $contact = $params['clientdetails']['phonenumber'];
-    
+
     // System Parameters
     $whmcsVersion = $params['whmcsVersion'];
+    $razorpayWHMCSVersion = RAZORPAY_WHMCS_VERSION;
     $callbackUrl = $params['systemurl'] . '/modules/gateways/callback/razorpay.php';
     $checkoutUrl = 'https://checkout.razorpay.com/v1/checkout.js';
+
+    $razorpayOrderId = createRazorpayOrderId($params);
 
     return <<<EOT
 <form name="razorpay-form" id="razorpay-form" action="$callbackUrl" method="POST">
@@ -80,14 +185,19 @@ function razorpay_link($params)
         data-key            = "$keyId"
         data-amount         = "$amount"
         data-currency       = "$currencyCode"
+        data-order_id       = "$razorpayOrderId"
         data-description    = "Inv#$invoiceId"
 
         data-prefill.name   = "$name"
         data-prefill.email  = "$email"
         data-prefill.contact= "$contact"
 
-        data-notes.whmcs_invoice_id = "$invoiceId"
-        data-notes.whmcs_version = "$whmcsVersion"
+        data-notes.whmcs_order_id = "$invoiceId"
+        data-notes.whmcs_version  = "$whmcsVersion"
+
+        data-_.integration                = "whmcs"
+        data-_.integration_version        = "$razorpayWHMCSVersion"
+        data-_.integration_parent_version = "$whmcsVersion"
     ></script>
 </form>
 EOT;
